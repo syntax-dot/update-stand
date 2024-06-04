d818259682b13302695c971e3fa0e8d163e4af91
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2024-06-04 10:16:45 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'org.capacitor.quasar.app',
+  appName: 'update-stand',
+  webDir: 'www'
+}
+
+export default config
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+
+import { addPushNotificationsListeners, registerNotifications } from 'src/utils/push-notifications'
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  await addPushNotificationsListeners()
+  await registerNotifications()
+})
+
+</script>
